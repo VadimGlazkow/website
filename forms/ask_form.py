2f82fd056ad8ae4c1b18b3a08737ab1245024577
VadimGlazkowUr <glazkowa.elena80.75@gmail.com>
2021-04-10 16:33:49 +0300

    Организована регистрация и вход. Дизайн нулевой

--- forms/ask_form.py
+++ forms/ask_form.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, SubmitField, BooleanField, StringField, FileField
+from wtforms import SubmitField, StringField, FileField, TextAreaField
 from wtforms.validators import DataRequired
 
 
 class AskForm(FlaskForm):
     title = StringField('Заголовок', validators=[DataRequired()])
-    question = StringField('Вопрос', validators=[DataRequired()])
+    question = TextAreaField('Вопрос', validators=[DataRequired()])
     fon_photo = FileField('Загрузить фон', default='fon.jpg')
     submit = SubmitField('Создать')
--- forms/search_form.py
+++ forms/search_form.py
@@ -1,7 +1,5 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-# TextAreaField - нужно будет при добавлении вопроса
 
 
 class SearchForm(FlaskForm):
